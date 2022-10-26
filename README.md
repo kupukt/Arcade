fb963eadb5d4b699a95704eea54e34c6a56e0ead
Kupu Tuifua <tui20002@byui.edu>
2022-10-26 10:00:54 -0600

    Files for arcade game.

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# Arcade
-Trying out pygame and making some sort of arcade game with it.
+
+
+# Overview
+
+Game developement was always something that I thought I'd be into because I have been playing video games for most of my life. It was a lot harder than I thought it would be and it takes so much time. I used arcade through python which is a framework for building games. Using this framework made everything about making a game so much easier. I decided to do something really simple which is a fireman catching fire and putting it out. Which is really just a moving sprite colliding with another sprite and removing the fire.
+
+[Software Demo Video](http://youtube.link.goes.here)
+
+# Development Environment
+
+The tools that I used for this game were really simple, just the python arcade framework and simple python code.
+
+# Useful Websites
+
+{Make a list of websites that you found helpful in this project}
+* [Real Python](https://realpython.com/arcade-python-game-framework/#arcade-concepts)
+* [Arcade Academy](https://learn.arcade.academy/en/latest/chapters/25_sprites_and_walls/sprites_and_walls.html)
+
+# Future Work
+
+{Make a list of things that you need to fix, improve, and add in the future.}
+* I want to be able to make a standing platform that will disappear when it is hit too many times with the fireballs.
+* Put in sounds for different collisions.
+* Move the platforms back and forth.
+* Apply gravity to the player.
---
+++ game.py
@@ -0,0 +1,192 @@
+from os import remove
+import arcade
+import random
+
+# Constants
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+SCREEN_TITLE = "FireFall"
+SCALING = 1.0
+GRAVITY = 5
+
+class FireFall(arcade.Window):
+    
+    # Class to handle the game
+    
+
+    def __init__(self, width, height, title):
+        """Initialize the game
+        """
+        super().__init__(width, height, title)
+
+        # Set up the empty sprite lists
+        self.fire_list = arcade.SpriteList()
+        self.platform_list = arcade.SpriteList()
+        self.all_sprites = arcade.SpriteList()
+
+    def setup(self):
+        # Get the game ready to play
+        
+
+        # Set the background color
+        arcade.set_background_color(arcade.color.ASH_GREY)
+
+        # Set up the player
+        self.player = arcade.Sprite("images/firefighter.png")
+        self.player.center_y = 90
+        self.player.left = 400
+        self.all_sprites.append(self.player)
+
+        platform = arcade.Sprite("images/ice_platform.png")
+
+        platform.center_y = self.height - (self.height)
+        platform.left = 200
+        platform.velocity = (0,0)
+        self.all_sprites.append(platform)
+        self.platform_list.append(platform)
+
+        arcade.schedule(self.add_fire, 1.0)
+        
+        self.physics_engine = arcade.PhysicsEnginePlatformer(
+            self.player, gravity_constant=GRAVITY
+        )
+
+        self.paused = False
+    
+    def add_fire(self, delta_time: float):
+       
+
+        # First, create the new enemy sprite
+        fire = MovingSprites("images/fireball.png")
+
+        # Set its position to a random height and off screen right
+        fire.left = random.randint(0, self.width)
+        fire.top = random.randint(self.height, self.height + 30)
+
+        fire.velocity = ((random.randint(-100,100)) , (random.randint(-200, -50 )))
+
+        self.fire_list.append(fire)
+        self.all_sprites.append(fire)
+
+    def add_platform(self, delta_time: float):
+
+        platform = arcade.Sprite("images/ice_platform.png", SCALING)
+
+        platform.center_y = self.height / 2
+        platform.left = 10
+        platform.velocity = (0,0)
+        self.all_sprites.append(platform)
+        self.platform_list.append(platform)
+      
+
+    def on_key_press(self, key: int, modifiers: int):
+        
+        if key == arcade.key.Q:
+            # Quit immediately
+            arcade.close_window()
+
+        if key == arcade.key.P:
+            self.paused = not self.paused
+
+        if key == arcade.key.W or key == arcade.key.UP:
+            self.player.change_y = 350
+            
+
+        if key == arcade.key.S or key == arcade.key.DOWN:
+            self.player.change_y = -350
+            
+
+        if key == arcade.key.A or key == arcade.key.LEFT:
+            self.player.change_x = -350
+
+        if key == arcade.key.D or key == arcade.key.RIGHT:
+            self.player.change_x = 350
+
+    def on_key_release(self, key: int, modifiers: int):
+        """Undo movement vectors when movement keys are released
+
+        Arguments:
+            symbol {int} -- Which key was pressed
+            modifiers {int} -- Which modifiers were pressed
+        """
+        if (
+            key == arcade.key.W
+            or key == arcade.key.S
+            or key == arcade.key.UP
+            or key == arcade.key.DOWN
+
+        ):
+            self.player.change_y = GRAVITY
+
+        if (
+            key == arcade.key.A
+            or key == arcade.key.D
+            or key == arcade.key.LEFT
+            or key == arcade.key.RIGHT
+        ):
+            self.player.change_x = 0
+    
+    def on_update(self, delta_time: float):
+     
+        # Check if player collides with fire, if so, remove the fire.
+        check_fire_collision = arcade.check_for_collision_with_list(self.player, self.fire_list)
+        for fire in check_fire_collision:
+            fire.remove_from_sprite_lists()
+        
+        
+        # Update everything
+        for sprite in self.all_sprites:
+            sprite.center_x = int(
+                sprite.center_x + sprite.change_x * delta_time
+            )
+            sprite.center_y = int(
+                sprite.center_y + sprite.change_y * delta_time
+            )
+        
+
+        # Keep player on screen
+        if self.player.top > self.height:
+            self.player.top = self.height
+        if self.player.right > self.width:
+            self.player.right = self.width
+        if self.player.bottom < 0:
+            self.player.bottom = 0
+        if self.player.left < 0:
+            self.player.left = 0
+
+    def on_draw(self):
+        """Draw all game objects"""
+
+        arcade.start_render()
+        self.all_sprites.draw()
+
+    
+
+class MovingSprites(arcade.Sprite):
+
+    def update(self):
+        
+
+        # Move the sprite
+        super().update()
+
+        # Remove us if we're off screen
+        if self.top > self.height + 50:
+            self.remove_from_sprite_lists()
+        elif self.right > self.width + 50:
+            self.remove_from_sprite_lists()
+        elif self.left < 0:
+            self.remove_from_sprite_lists()
+        elif self.bottom < 0:
+            self.remove_from_sprite_lists()
+
+
+if __name__ == "__main__":
+    
+    game = FireFall(
+        int(SCREEN_WIDTH * SCALING), int(SCREEN_HEIGHT * SCALING), SCREEN_TITLE
+    )
+    
+    game.setup()
+   
+    arcade.run()
